a22ab21ae10aab59324e981eda94a2031c655142
Java <Java@DESKTOP-04KTJS7>
2020-01-11 18:48:56 +0900

    initial commit

---
+++ src/Gugudan_45.java
@@ -0,0 +1,44 @@
+import java.util.Scanner;
+
+public class Gugudan_45 {
+
+	public static void main(String[] args) {
+		
+		//4dan
+		System.out.println("4dan");
+		int result = 4 * 1;
+		System.out.println(result);
+		
+		result = 4 * 2;
+		System.out.println(result);
+		result = 4 * 3;
+		System.out.println(result);
+		result = 4 * 4;
+		System.out.println(result);
+		result = 4 * 5;
+		System.out.println(result);
+		result = 4 * 6;
+		System.out.println(result);
+		result = 4 * 7;
+		System.out.println(result);
+		result = 4 * 8;
+		System.out.println(result);
+		result = 4 * 9;
+		System.out.println(result);
+		
+		//5dan
+		System.out.println("구구단 중 출력할 단은? : ");
+		Scanner scanner = new Scanner(System.in);
+		int number = scanner.nextInt();
+		System.out.println(number * 1);
+		System.out.println(number * 2);
+		System.out.println(number * 3);
+		System.out.println(number * 4);
+		System.out.println(number * 5);
+		System.out.println(number * 6);
+		System.out.println(number * 7);
+		System.out.println(number * 8);
+		System.out.println(number * 9);
+	}
+
+}
